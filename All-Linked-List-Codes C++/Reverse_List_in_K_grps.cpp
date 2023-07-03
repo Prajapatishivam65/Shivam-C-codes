0f740533a02293bf435d23783f44c62a27e1b885
Prajapatishivam65 <113005045+Prajapatishivam65@users.noreply.github.com>
2023-07-03 21:41:56 +0530

    Add files via upload

---
+++ All-Linked-List-Codes C++/Reverse_List_in_K_grps.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+class Node {
+public:
+    int data;
+    Node* next;
+    Node() : data(0), next(nullptr) {}
+    Node(int x) : data(x), next(nullptr) {}
+    Node(int x, Node* next) : data(x), next(next) {}
+};
+
+Node* kReverse(Node* head, int k) {
+    //base call
+    if(head == NULL) {
+        return NULL;
+    }
+    
+    //step1: reverse first k nodes
+    Node* next = NULL;
+    Node* curr = head;
+    Node* prev = NULL;
+    int count= 0;
+    
+    while( curr != NULL && count < k ) {
+        next = curr -> next;
+        curr -> next = prev;
+        prev = curr;
+        curr = next;
+        count++;
+    }
+    if (count < k) {
+      curr = prev;
+      next = NULL;
+      prev = NULL;
+       count = 0;
+      while (curr != NULL && count < k) {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+        count++;
+      }
+    }
+    //step2: Recursion dekhlega aage ka 
+    if(next != NULL) {
+        head -> next = kReverse(next,k);
+    }
+    
+    //step3: return head of reversed list
+    return prev;
+}
+
+int main() {
+    // Create the linked list
+    Node* head = new Node(5);
+    head->next = new Node(4);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(7);
+    head->next->next->next->next = new Node(9);
+    head->next->next->next->next->next = new Node(2);
+
+    // Reverse every 4 nodes in the list
+    int k = 3;
+    head = kReverse(head, k);
+
+    // Print the reversed list
+    Node* temp = head;
+    while (temp != NULL) {
+        std::cout << temp->data << " ";
+        temp = temp->next;
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
